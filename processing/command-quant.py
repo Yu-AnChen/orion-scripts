faac4be4c742b10ce0a86124a380375f1b9fff90
Yu-An Chen <atwood12@gmail.com>
2023-03-03 22:29:44 -0800

    Update quantification running script

--- processing/command-quant.py
+++ processing/command-quant.py
@@ -1,90 +1,100 @@
-import sys
-sys.path.append(r'C:\Users\Public\Downloads\quantification')
-
-import os
-import pathlib
+import argparse
 import datetime
+import pathlib
+import subprocess
+import sys
 import time
 
-def main(slide_dir, ome_tiff_path, marker_csv_path):
+import run_all_utils
 
-    target_dir = slide_dir
-    target_dir = pathlib.Path(target_dir)
 
-    masks = [
-        'cell.ome.tif',
-        # 'nuclei.ome.tif'
-    ]
+command = '''
+python ../modules/quantification/CommandSingleCellExtraction.py
+    --masks {}
+    --image "{}" 
+    --output "{}"
+    --channel_names "{}"
+'''
 
-    mask_paths = [
-        sorted((target_dir / 'segmentation').rglob(f"{m}*"))[0]
-        for m in masks
-    ]
+MODULE_NAME = 'quantification'
+ORION_DEFAULTS = [
+    ('masks name pattern', '*cellRing*.ome.tif', ''),
+    ('channel_names', None, '')
+]
 
-    import pipeline
-    # import pyimagej_rolling_ball
+
+def main(argv=sys.argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c',
+        metavar='config-csv',
+        required=True
+    )
+    parser.add_argument(
+        '-m',
+        metavar='module-params',
+        required=False,
+        default=None
+    )
+    parsed_args = parser.parse_args(argv[1:])
     
-    # p = pipeline.Pipeline(
-    #     mask_paths=mask_paths,
-    #     output_dir=target_dir / 'quantification' / 'ij_rb_100',
-    #     img_path=ome_tiff_path,
-    #     marker_csv_path=marker_csv_path,
-    #     img_preprocess_func=pyimagej_rolling_ball.ij_rolling_ball_dask,
-    #     img_preprocess_kwargs=dict(radius=100, verbose=False),
-    #     table_prefix='ij_rb_100-',
-    #     save_RAM=False
-    # )
+    CURR = pathlib.Path(__file__).resolve().parent
 
-    q = pipeline.Pipeline(
-        mask_paths=mask_paths,
-        output_dir=target_dir / 'quantification' / 'raw',
-        img_path=ome_tiff_path,
-        marker_csv_path=marker_csv_path,
-        img_preprocess_func=None,
-        img_preprocess_kwargs=None,
-        table_prefix='',
-        save_RAM=False,
-        skip=None
+    file_config, module_params, log_path = run_all_utils.init_run(
+        parsed_args, ORION_DEFAULTS, MODULE_NAME
     )
 
-    start_time = int(time.perf_counter())
-    print(datetime.datetime.now(), '\n')
+    assert module_params['channel_names'] is not None, 'channel_names is required'
+    marker_csv_path = pathlib.Path(module_params['channel_names'])
+    assert marker_csv_path.exists(), f"{marker_csv_path} does not exist"
 
-    # p.run()
-    q.run()
+    commands = []
+    for config in file_config[:]:
+        config = run_all_utils.set_config_defaults(config)
 
-    end_time = int(time.perf_counter())
-    print('elapsed', datetime.timedelta(seconds=end_time-start_time))
-    print('')
+        name = config['name']
+        out_dir = config['out_dir']
+
+        print('Processing', name)
+
+        masks = module_params['masks name pattern'].split(',')
+        img_path = pathlib.Path(config['path'])
+        img_stem = img_path.name.split('.')[0]
+        mask_dir = out_dir / name / 'segmentation' / img_stem
+        try:
+            mask_paths = [sorted(mask_dir.glob(m))[0] for m in masks]
+        except IndexError as e:
+            print(f"\nError: {mask_dir} does not contain file with name pattern {masks}\n")
+            raise(e)
+        mask_paths = [f'"{p}"' for p in mask_paths]
+
+        command_run = [
+            'python',
+            CURR.parent / 'modules/quantification/CommandSingleCellExtraction.py',
+            '--image', config['path'],
+            '--output', out_dir / name / 'quantification',
+            '--masks',
+        ]
+        command_run.extend(mask_paths)
+        for kk, vv in module_params.items():
+            if ' ' not in kk:
+                command_run.extend([f"--{kk}", str(vv)])
+
+        commands.append(command_run)
+        # start_time = int(time.perf_counter())
+        # subprocess.run(command_run)
+        # end_time = int(time.perf_counter())
+
+        # print('elapsed', datetime.timedelta(seconds=end_time-start_time))
+        # print()
+
+        # run_all_utils.to_log(
+        #     log_path, config['path'], end_time-start_time, module_params
+        # )
+
+    return 0
 
 
-import pathlib
-import csv
-
-with open(r'C:\Users\Public\Downloads\orion-scripts\file_list.csv') as file_config_csv:
-    csv_reader = csv.DictReader(file_config_csv)
-    file_config = [dict(row) for row in csv_reader]
-
-group_dir = pathlib.Path(r'C:\rcpnl\mcmicro')
-ome_dir = pathlib.Path(r'C:\rcpnl\tissue')
-
-marker_csv_path = r'C:\Users\Public\Downloads\orion-scripts\markers.csv'
-
-names = [c['name'] for c in file_config]
-slide_dirs = [
-    group_dir / n 
-    for n in names
-]
-
-ome_tiffs = [
-    ome_dir / c['path']
-    for c in file_config
-]
-
-
-for s, o in zip(slide_dirs[:], ome_tiffs[:]):
-    print(slide_dirs)
-    print(ome_tiffs)
-    print('Processing', s.name)
-    main(str(s), str(o), marker_csv_path)
-    print()
+if __name__ == '__main__':
+    sys.exit(main())
